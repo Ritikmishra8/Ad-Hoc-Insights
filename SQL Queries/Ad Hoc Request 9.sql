0f23f8f7be9dad320d0238ccd085913af1955cf7
Ritik Mishra <ritikmishra8july@gmail.com>
2025-07-15 11:42:54 +0530

    Update Ad Hoc Request 9.sql

--- SQL Queries/Ad Hoc Request 9.sql
+++ SQL Queries/Ad Hoc Request 9.sql
@@ -2,14 +2,14 @@
  # percentage of contribution? The final output contains these fields: channel, gross_sales_mln, percentage.
  
  with cte as (select channel ,
-			round(sum(sold_quantity* gross_price)/1000000,2) as gross_sales_mln
-			from dim_customer c
-			join fact_sales_monthly s
-			using(customer_code)
-			join fact_gross_price g
-			on g.product_code =s.product_code
-			where s.fiscal_year =2021
-			group by channel)
+	round(sum(sold_quantity* gross_price)/1000000,2) as gross_sales_mln
+	from dim_customer c
+	join fact_sales_monthly s
+	using(customer_code)
+	join fact_gross_price g
+	on g.product_code =s.product_code
+	where s.fiscal_year =2021
+	group by channel)
 select *,
 concat(round(gross_sales_mln * 100/sum(gross_sales_mln) over() ,2),"%") as percentage_contribution
 from cte
