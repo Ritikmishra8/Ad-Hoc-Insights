9a6360d98744a7fe3f78f432769fb72e1f734b6a
Ritik Mishra <ritikmishra8july@gmail.com>
2025-07-15 11:06:18 +0530

    Update Ad Hoc Request 1.sql

--- SQL Queries/Ad Hoc Request 1.sql
+++ SQL Queries/Ad Hoc Request 1.sql
@@ -1,8 +1,8 @@
 ## Q1: Provide list of markets in which customer "Atliq Exclusive" operates its business in the APAC region.
 
-```sql
+sql
 SELECT DISTINCT market
 FROM dim_customer
 WHERE customer = "Atliq Exclusive"
   AND region = "APAC";
-```
+
